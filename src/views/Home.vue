b73793e73db8232c52bc8e619af0378f9cb359fc
hkim <hkim@girdwiz.com>
2019-01-17 16:03:35 +0900

    test3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcomeddddfasdfsd to Your Vue.js App"/>
+    <HelloWorld msg="Welcomeddddfasdfsddfffffffd to Your Vue.js App"/>
   </div>
 </template>
 
