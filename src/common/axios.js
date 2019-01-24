e9ce1b56096597ee5f17b8d013eec03a37126fde
lyjguy <lyjguy@gridwiz.com>
2019-01-24 16:36:51 +0900

    export 선언 변경

--- src/common/axios.js
+++ src/common/axios.js
@@ -4,16 +4,14 @@ axios.defaults.baseURL = `${process.env.VUE_APP_TOS_WEB_URL}`;
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-const tosWebAxios = axios.create({
+export const tosWebAxios = axios.create({
   baseURL: `${process.env.VUE_APP_TOS_WEB_URL}`,
 });
 
-const tosAuthAxios = axios.create({
+export const tosAuthAxios = axios.create({
   baseURL: `${process.env.VUE_APP_TOS_AUTH_URL}`,
 });
 
-const tosApiAxios = axios.create({
+export const tosApiAxios = axios.create({
   baseURL: `${process.env.VUE_APP_TOS_API_URL}`,
 });
-
-export { tosWebAxios, tosAuthAxios, tosApiAxios };
