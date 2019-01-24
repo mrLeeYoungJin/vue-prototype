d1323f8bf4e1af28f2f179d981f7b07562a9d949
lyjguy <lyjguy@gridwiz.com>
2019-01-24 11:20:16 +0900

    공통 mutation 상수 데이타 파일 추가

---
+++ src/store/mutation-types.js
@@ -0,0 +1,8 @@
+// user
+export const SET_USER = 'SET_USER';
+export const SET_ACCESS_TOKEN = 'SET_ACCESS_TOKEN';
+export const SET_REFRESH_TOKEN = 'SET_REFRESH_TOKEN';
+
+// system
+
+export const SET_UUID = 'SET_UUID';
