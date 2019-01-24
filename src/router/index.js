aa445b210c568fa88c65001eb8dd9ad7db2b59fa
lyjguy <lyjguy@gridwiz.com>
2019-01-24 11:24:06 +0900

    정보 업데이트

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,13 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
+import Login from '@/views/Login.vue';
+
+// Other
+import NotFound from '@/views/NotFound.vue'
+
+import { getUUID, isEmpty } from '@/common/utilities';
+import * as localStore from '@/common/local-storage';
 
 Vue.use(Router);
 
@@ -9,44 +16,71 @@ const router = new Router({
   mode: 'history',
   routes: [
     {
+        path: '*',
+        component: NotFound,
+        name: 'notFound',
+        meta: {title: 'Not Found'}
+    },
+    {
       path: '/',
-      name: 'home',
+      name: 'main',
       component: Home,
-      meta: {title: 'Home'},
+      meta: { title: 'Home', requiresAuth: true },
     },
     {
       path: '/about',
       name: 'about',
       component: About,
-      meta: {title: 'About'},
+      meta: { title: 'About', requiresAuth: true },
     },
     {
       path: '/dashboard',
       name: 'dashboard',
       component: Home,
-      meta: {title: 'dashboard', requiresAuth: true},
+      meta: { title: 'dashboard', requiresAuth: true },
       children: [
         { path: '', component: Home },
-        { path: 'singleDevice', component: Home, meta: {title: 'singleDevice'} },
-        { path: 'deviceStatus', component: Home, meta: {title: 'deviceStatus'} },
-      ]
+        { path: 'singleDevice', component: Home, meta: { title: 'singleDevice' } },
+        { path: 'deviceStatus', component: Home, meta: { title: 'deviceStatus' } },
+      ],
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: { title: 'Login' },
+    },
+    {
+      path: '/logout',
     },
   ],
 });
 
 router.beforeEach((to, from, next) => {
-    // Start our vue-progressbar
-    router.app.$Progress.start()
+  // Start our vue-progressbar
+  router.app.$Progress.start();
 
-    // To set the title of each route
-    document.title = to.meta.title
+  document.title = to.meta.title;
 
-    next()
-})
+  // deviceUUID 생성 및 확인
+  router.app.$options.store.dispatch('system/getSystemUuid');
+  const accessToken = localStore.getAccessToken();
+
+  if (!isEmpty(accessToken)) {
+    router.app.$options.store.dispatch('user/setUserAndTokens', { accessToken });
+  }
+
+  if (to.meta.requiresAuth && isEmpty(router.app.$options.store.getters['user/user'])) {
+    next({ name: 'login' });
+    document.location.href = '/login';
+  } else {
+    next();
+  }
+});
 
 router.afterEach((to, from) => {
-    // End our vue-progressbar
-    router.app.$Progress.finish()
-})
+  // End our vue-progressbar
+  router.app.$Progress.finish();
+});
 
 export default router;
