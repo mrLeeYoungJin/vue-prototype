7c7641b30f62ab001a4e6b8d4992364bc8f635a2
lyjguy <lyjguy@gridwiz.com>
2019-01-25 18:55:49 +0900

    uri업데이트

--- src/common/uriinfo.js
+++ src/common/uriinfo.js
@@ -4,3 +4,4 @@ export const USER_URI_PATH = '/users';
 // auth api
 export const USER_AUTH_LOGIN_PATH = '/auth/login';
 export const USER_AUTH_LOGOUT_PATH = '/auth/logout';
+export const USER_AUTH_UPDATE = '/auth/update';
