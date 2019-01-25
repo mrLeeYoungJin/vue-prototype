a10cdd76323ddaa9f206e0f742821cecc2d16b27
lyjguy <lyjguy@gridwiz.com>
2019-01-25 18:55:30 +0900

    http status code 관리 파일 추가

---
+++ src/common/statusinfo.js
@@ -0,0 +1,14 @@
+export const ACCESS_TOKEN_EXPIRED = {
+  status: 401,
+  message: 'ACCESS_TOKEN_EXPIRED',
+};
+
+export const REFRESH_TOKEN_EXPIRED = {
+  status: 401,
+  message: 'REFRESH_TOKEN_EXPIRED',
+};
+
+export const UNAUTHENTICATED = {
+  status: 401,
+  message: 'UNAUTHENTICATED',
+};
